b87abb48c5af6e31680800a07b294bac90fdf438
Nands17 <1singhnan@gmail.com>
2024-11-25 22:57:13 +0530

    Update Library management project.py

--- Library management project.py
+++ Library management project.py
@@ -1,7 +1,14 @@
-cq
 #For security purposes we use a password
 password=input("Enter password:")
-if password=="123@abclibrary":
-    print("hi")
+if password=="123@novelnestlibrary":
+    data={}
+    #for the main page
+    name="Novel Nest Library"
+    sign=input("Sign in or sign up:")
+    if sign=="Sign in":
+        user=input("Username:")
+        password=input("Password:")
+    elif sign=="Sign up":
+        user=enter 
 else:
     print("Wrong password! Terminating.")
